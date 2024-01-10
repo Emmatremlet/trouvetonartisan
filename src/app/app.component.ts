719c1275da0f8e2f0ae136ddba19b7b2ced163da
Emma TREMLET <elecuelle001@ensc.fr>
2024-01-10 17:41:37 +0100

    feat: :sparkles: add a footer and start the home page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,6 +12,7 @@ import { RouterOutlet } from '@angular/router';
 export class AppComponent {
   title = 'trouve-ton-artisan';
   logo: any = "../assets/logo.png";
-  search: any = "../assets/search.png"
-  picture: any = "../assets/Visuel-flou.jpeg"
+  search: any = "../assets/search.png";
+  picture: any = "../assets/Visuel-flou.jpeg";
+  location: any = "../assets/location.png";
 }
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,12 +1,15 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-hom',
+  selector: 'app-home',
   standalone: true,
   imports: [],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
-
+  location: any = "../assets/location.png";
+  star: any = "../assets/star.png";
+  halfStar: any ="../assets/half-star.png"
+  favicon: any ="../assets/favicon.png"
 }
