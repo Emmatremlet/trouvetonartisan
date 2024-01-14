c08caf814a104fec5ac438fcb923c36357256830
Emma TREMLET <elecuelle001@ensc.fr>
2024-01-14 19:34:25 +0100

    feat: :sparkles: add 404 Not Found page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,6 +2,7 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { ArtisanComponent } from './artisan/artisan.component';
 import { ArtisansSheetComponent } from './artisans-sheet/artisans-sheet.component';
+import { NotFoundComponent } from './not-found/not-found.component';
 
 export const routes: Routes = [
     { path: "", component: HomeComponent },
@@ -11,7 +12,8 @@ export const routes: Routes = [
     { path: 'alimentation', component: ArtisanComponent, data: { category: 'Alimentation' } },
     { path: 'batiment', component: ArtisanComponent, data: { category: 'Bâtiment' } },    
     { path: "artisansheet/:id", component: ArtisansSheetComponent },
-    { path: '', redirectTo: '/', pathMatch: 'full' },
+    { path: 'not-found', component: NotFoundComponent },
+    { path: '**', redirectTo: '/not-found' }
 ];
     
 export class AppRoutingModule { }
