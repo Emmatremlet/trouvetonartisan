d1ac238895f60218f3d9979faec9b10e1ac48def
Emma TREMLET <elecuelle001@ensc.fr>
2024-02-16 10:51:04 +0100

    style: test

--- src/app/artisan/artisan.component.ts
+++ src/app/artisan/artisan.component.ts
@@ -12,12 +12,12 @@ import { ChangeDetectorRef } from '@angular/core';
   templateUrl: './artisan.component.html',
   styleUrl: './artisan.component.css',
 })
-export class ArtisanComponent implements OnInit{
+export class ArtisanComponent implements OnInit {
   location: any = '../assets/location.png';
   search: any = '../assets/search.png';
   favicon: any = '../assets/favicon.png';
   @Input() id: number = 0;
-  artisans!: Artisan [];
+  artisans!: Artisan[];
   sortedArtisans!: Artisan[];
   selectedCategory: string = '';
   searchTerm: string = '';
@@ -25,10 +25,12 @@ export class ArtisanComponent implements OnInit{
 
   artisan: Artisan | undefined;
 
-  
-
-  constructor(private artisanService: ArtisansService, private route: ActivatedRoute, private cdRef: ChangeDetectorRef) {
-    this.artisanService.getArtisans().subscribe(artisans => {
+  constructor(
+    private artisanService: ArtisansService,
+    private route: ActivatedRoute,
+    private cdRef: ChangeDetectorRef,
+  ) {
+    this.artisanService.getArtisans().subscribe((artisans) => {
       console.log(artisans);
       this.artisans = artisans;
       this.sortedArtisans = [...this.artisans];
@@ -36,33 +38,44 @@ export class ArtisanComponent implements OnInit{
   }
 
   searchArtisans(event: Event) {
+    console.log("On rentre dans la fonction searchArtisans");
     const target = event.target as HTMLSelectElement;
     this.searchTerm = target.value;
-    this.sortedArtisans = this.artisans.filter(artisan =>
-      artisan.name.toLowerCase().includes(target.value.toLowerCase())
+    this.sortedArtisans = this.artisans.filter((artisan) =>
+      artisan.name.toLowerCase().includes(target.value.toLowerCase()),
+    );
+    this.sortedArtisans = this.artisans.filter((artisan) =>
+      artisan.specialty.toLowerCase().includes(target.value.toLowerCase()),
+    );
+    this.sortedArtisans = this.artisans.filter((artisan) =>
+      artisan.location.toLowerCase().includes(target.value.toLowerCase()),
     );
   }
 
-  sortArtisans(event: Event) {
-    const target = event.target as HTMLSelectElement;
-    this.selectedCategory = target.value;
-  
+  sortArtisans() {
+    console.log('on rentre dans la fonction sortArtisans');
+    console.log(this.selectedCategory);
+
     if (this.selectedCategory) {
-      this.sortedArtisans = this.artisans.filter(artisan =>
-        artisan.category === this.selectedCategory
-      );
+      this.filterArtisansByCategory(this.selectedCategory);
     } else {
-      this.sortedArtisans = [...this.artisans];
+      this.artisanService.getArtisans().subscribe((artisans) => {
+        this.artisans = artisans;
+        this.sortedArtisans = [...this.artisans];
+        this.cdRef.detectChanges();
+      });
     }
   }
-  filterArtisansByCategory(category: string): void {
-    this.artisanService.getArtisans().subscribe(artisans => {
-      this.sortedArtisans = artisans.filter(artisan => artisan.category === category);
-      this.cdRef.detectChanges();
 
+  filterArtisansByCategory(category: string): void {
+    this.artisanService.getArtisans().subscribe((artisans) => {
+      this.sortedArtisans = artisans.filter(
+        (artisan) => artisan.category === category,
+      );
+      this.cdRef.detectChanges();
     });
   }
-  
+
   ngOnInit(): void {
     const category = this.route.snapshot.data['category'];
     if (category) {
@@ -75,5 +88,4 @@ export class ArtisanComponent implements OnInit{
       });
     }
   }
-  
 }
