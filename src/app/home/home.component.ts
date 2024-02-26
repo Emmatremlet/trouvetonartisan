5e1aebc9ee12338703e80651a7ec0351e83db1ee
Emma TREMLET <elecuelle001@ensc.fr>
2024-02-26 18:24:02 +0100

    feat: :sparkles: Add-the-artisan-with-the-best-note-in-home-page

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,15 +1,111 @@
 import { Component } from '@angular/core';
+import { ArtisansService } from '../artisans.service';
+import { CommonModule } from '@angular/common';
+import { Artisan } from '../artisan.model';
+
+declare var google: any;
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.css'
+  styleUrl: './home.component.css',
 })
 export class HomeComponent {
-  location: any = "../assets/location.png";
-  star: any = "../assets/star.png";
-  halfStar: any ="../assets/half-star.png"
-  favicon: any ="../assets/favicon.png"
+  location: any = '../assets/location.png';
+  favicon: any = '../assets/favicon.png';
+  halfStar: any = '../assets/half-star.png';
+  artisans!: Artisan[];
+  bestArtisan: any;
+  secondArtisan: any;
+  thirdArtisan: any;
+  stars1: any;
+  fullnote1: any[] = [];
+  halfnote1: any[] = [];
+  halfStars1: any;
+  decimal1: any;
+  stars2: any;
+  fullnote2: any[] = [];
+  halfnote2: any[] = [];
+  halfStars2: any;
+  decimal2: any;
+  stars3: any;
+  fullnote3: any[] = [];
+  halfnote3: any[] = [];
+  halfStars3: any;
+  decimal3: any;
+  location3: any;
+  map3: any;
+
+  constructor(private artisanService: ArtisansService) {}
+
+  ngOnInit(): void {
+    this.artisanService.getArtisans().subscribe((artisans: any[]) => {
+      artisans.sort((a, b) => b.note - a.note);
+      this.bestArtisan = artisans[0];
+      this.secondArtisan = artisans[1];
+      this.thirdArtisan = artisans[2];
+    });
+    if (this.bestArtisan && this.bestArtisan.note) {
+      this.stars1 = Math.floor(this.bestArtisan.note);
+      this.decimal1 = this.bestArtisan.note - Math.floor(this.bestArtisan.note);
+
+      if (this.decimal1 != 0) {
+        this.halfStars1 = 1;
+      }
+      this.fullnote1 = Array(this.stars1).fill(0);
+      this.halfnote1 = Array(this.halfStars1).fill(0);
+    }
+
+    if (this.secondArtisan && this.secondArtisan.note) {
+      this.stars2 = Math.floor(this.secondArtisan.note);
+      this.decimal2 = this.bestArtisan.note - Math.floor(this.bestArtisan.note);
+
+      if (this.decimal2 != 0) {
+        this.halfStars2 = 1;
+      }
+      this.fullnote2 = Array(this.stars2).fill(0);
+      this.halfnote2 = Array(this.halfStars2).fill(0);
+    }
+
+    if (this.thirdArtisan && this.thirdArtisan.note) {
+      this.stars3 = Math.floor(this.thirdArtisan.note);
+      this.decimal3 =
+        this.thirdArtisan.note - Math.floor(this.thirdArtisan.note);
+
+      if (this.decimal3 != 0) {
+        this.halfStars3 = 1;
+      }
+      this.fullnote3 = Array(this.stars3).fill(0);
+      this.halfnote3 = Array(this.halfStars3).fill(0);
+      this.location3 = this.thirdArtisan.location;
+      this.initMap(this.thirdArtisan, "map3");
+    }
+  }
+
+  initMap(artisan: any, id: any): void {
+    const map = new google.maps.Map(document.getElementById(id), {
+      center: { lat: 48.858599614043435, lng: 2.3621384610414586 },
+      zoom: 8,
+    });
+
+    const geocoder = new google.maps.Geocoder();
+    geocoder.geocode(
+      { address: artisan.location },
+      (results: any, status: any) => {
+        if (status === 'OK') {
+          const marker = new google.maps.Marker({
+            map: map,
+            position: results[0].geometry.location,
+            title: artisan.name,
+          });
+        } else {
+          console.error(
+            'Geocode was not successful for the following reason: ' + status,
+          );
+        }
+      },
+    );
+  }
 }
