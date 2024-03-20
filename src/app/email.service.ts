93873a4b73856f35e546061aa5a98e8689049f2b
Emma TREMLET <elecuelle001@ensc.fr>
2024-03-20 15:07:58 +0100

    feat: :construction: add evironment variables

--- src/app/email.service.ts
+++ src/app/email.service.ts
@@ -1,29 +1,28 @@
 import { Injectable } from '@angular/core';
-import * as emailjs from 'emailjs-com';
+import * as emailjs from '@emailjs/browser';
+import { environment } from '../environments/environment.development';
+
 
 @Injectable({
   providedIn: 'root',
 })
 
 //Service allowing you to send an email using emailjs
-  
 export class EmailService {
   constructor() {}
+  private serviceId: string = environment.EMAILJS_SERVICE_ID;
+  private templateId: string = environment.EMAILJS_TEMPLATE_ID;
+  private userId: string = environment.EMAILJS_USER_ID;
 
   sendEmail(templateParams: any): Promise<any> {
     return emailjs
-      .send(
-        'service_ptlbqr8',
-        'template_16vskto',
-        templateParams,
-        '-zAyczA1-KABOJJnO',
-      )
+      .sendForm(this.serviceId, this.templateId, templateParams, this.userId)
       .then(
-        (response) => {
+        (response: any) => {
           console.log('Success!', response.status, response.text);
           return response;
         },
-        (error) => {
+        (error: any) => {
           console.error('Failed...', error);
           throw error;
         },
---
+++ src/environments/environment.ts
@@ -0,0 +1,9 @@
+export const environment: {
+  EMAILJS_SERVICE_ID: string;
+  EMAILJS_TEMPLATE_ID: string;
+  EMAILJS_USER_ID: string;
+} = {
+   EMAILJS_SERVICE_ID : 'service_ptlbqr8',
+   EMAILJS_TEMPLATE_ID : 'template_16vskto',
+   EMAILJS_USER_ID : '-zAyczA1-KABOJJnO',
+};
